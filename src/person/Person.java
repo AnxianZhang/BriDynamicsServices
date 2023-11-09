bab2897f7108132c8ae88d1605d7dd31de3d2cf1
gadan <gadanxianzhang@gmail.com>
2023-11-09 15:24:01 +0100

    programmer interfdace

--- src/person/Person.java
+++ src/person/Person.java
@@ -1,26 +1,9 @@
 package person;
 
 public interface Person {
-//    private String login;
-//    private final String hashedPwd;
-//
-//    private final byte [] salt;
-//
-//    protected Person (String login, String pwd){
-//        this.login = login;
-//        this.salt = HashPassword.generateSalt();
-//        this.hashedPwd = HashPassword.getHashPassword(pwd, this.salt);
-//    }
-//
-//    public String getHashedPwd() {
-//        return hashedPwd;
-//    }
-//
-//    public boolean isSamePwd(String pdwIn){
-//        return this.hashedPwd.equals(HashPassword.getHashPassword(pdwIn, this.salt));
-//    }
-
     boolean isSameLogin(String login);
 
     boolean isSamePwd(String pdwIn);
+
+    String getLogin();
 }
